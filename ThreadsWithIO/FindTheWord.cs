c7a7e857d21807fb93ae17c398b45766fdece589
Oleksii Remizov <oleksii.remizov@synapse.com>
2017-10-10 17:23:14 +0300

    Threads

---
+++ ThreadsWithIO/FindTheWord.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ThreadsWithIO
+{
+    public class FindTheWord
+    {
+        public string curFile = "D:\\InformClientLog.txt";
+        public string curWord = "English";
+
+        public Dictionary<int, string> FindTheWordInTheText(string curFile, string curWord)
+        {
+            var wordsStat = new Dictionary<int, string>();
+            int amountInTheLine;
+            try
+            {
+                using (var streamReader = new StreamReader(curFile))
+                {
+
+                    int numberOfString = 0;
+                    string inputLine;
+
+                    while (!streamReader.EndOfStream)
+                    {
+                        inputLine = streamReader.ReadLine();
+                        numberOfString++;
+                        amountInTheLine = new Regex(curWord).Matches(inputLine).Count;
+                        if (amountInTheLine > 0)
+                        {
+                            wordsStat[numberOfString] = inputLine;
+                        }
+
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The programm failed with an error.");
+                Console.WriteLine(ex.ToString());
+            }
+            return wordsStat;
+        }
+    }
+}
---
+++ ThreadsWithIO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ThreadsWithIO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            FindTheWord ftw = new FindTheWord();
+
+            if (ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), ftw))
+            {
+                Console.WriteLine("Main Thread does some work");
+                Thread.Sleep(1000);
+
+                Console.WriteLine("Exit from main thread");
+            }
+            else
+                Console.WriteLine("Unable to queue ThreadPool request.");
+            Console.ReadKey();
+        }
+
+        static void ThreadProc(object stateInfo)
+        {
+            FindTheWord ftw = (FindTheWord)stateInfo;
+            foreach (var wordsNumber in ftw.FindTheWordInTheText(ftw.curFile, ftw.curWord).OrderByDescending(ws => ws.Value))
+            {
+                Console.WriteLine("Substring {0} is in line #{1} and occurs <<< {2} >>> ", ftw.curWord, wordsNumber.Key, wordsNumber.Value);
+            }
+
+
+        }
+    }
+}
